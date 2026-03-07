6da8fb4ff9fb0734717850337246eefdae315094
Alex5969 <alexandreadjcdesrousseaux@gmail.com>
2026-03-07 11:50:24 +0100

    Update secrets_config.php

--- secrets_config.php
+++ secrets_config.php
@@ -1,16 +1,1 @@
-<?php
-/* MySQL */ 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'vite_et_gourmand');
-define('DB_USER', 'root'); 
-define('DB_PASS', '');     
-
-/* MongoDB */
-define('MONGO_URI', 'mongodb://127.0.0.1:27017');
-define('MONGO_DB_NAME', 'vite_et_gourmand_stats');
-
-/* Sécurité & Email */
-define('ENCRYPTION_KEY', '8eb543d7e2f1a619c8b7d6e5f4a321b0'); 
-define('SMTP_HOST', 'smtp.mailtrap.io');
-define('SMTP_USER', 'user');
-define('SMTP_PASS', 'password');
+not safe here (but jury already have this part)
